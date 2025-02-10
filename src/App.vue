b584dabf7d419a6110fe1ad5cd1c548da0c6895d
alexchexes <alexchexes@gmail.com>
2025-02-10 21:29:39 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sm:bg-green-100 bg-blue-100">
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, dolore eius. Deleniti
+      dicta inventore excepturi, atque vel amet. Provident amet cupiditate tempore animi accusantium
+      et perferendis repudiandae corporis placeat optio?
+    </div>
+
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, dolore eius. Deleniti
+      dicta inventore excepturi, atque vel amet. Provident amet cupiditate tempore animi accusantium
+      et perferendis repudiandae corporis placeat optio?
+    </div>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import plugin from 'tailwindcss/plugin'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        html: { fontSize: '15px' },
+      })
+    }),
+  ],
+}
