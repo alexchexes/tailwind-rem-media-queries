cf20c459aa60a2c28308fc4ffe6781805366f8b6
alexchexes <alexchexes@gmail.com>
2025-02-10 21:32:32 +0000

    comment

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Green BG expected to be from 640px, not 40 rem !! -->
   <div class="sm:bg-green-100 bg-blue-100">
     <div>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, dolore eius. Deleniti
